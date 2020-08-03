a898e057240707be1885de39a794b9e978b1c298
Tim Coote <tim+github.com@coote.org>
2020-08-03 19:00:28 +0100

    removed dplyr and shiny. Broke ggvis

--- install.R
+++ install.R
@@ -2,8 +2,8 @@
 require(devtools)
 #install.packages ("ggvis")
 install_version("ggvis", version = "0.4.5")
-install_version("dplyr", version = "0.8.3")
-install_version("shiny", version = "1.4.0.2")
+#install_version("dplyr", version = "0.8.3")
+#install_version("shiny", version = "1.4.0.2")
 #install_version("htmltools", version = "0.4.0")
 
 library (magrittr)
